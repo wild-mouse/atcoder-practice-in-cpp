143d4307a1e8137bf4794c0b2055ff376303bc8d
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-18 07:59:38 +0900

    Add Transfer

---
+++ abc/136/transfer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ((a - b) > c)
+
+        cout << 0 << endl;
+    } else
+    {
+        cout << (c - (a - b)) << endl;
+    }
+
+    return 0;
+}
