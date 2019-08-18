393376c1d7242191b986d9c570a6b392a75f24fe
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-19 08:12:26 +0900

    Add Rounding

---
+++ abc/130/rounding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, a;
+    cin >> x >> a;
+    if (x >= a) {
+        cout << "10" << endl;
+    } else {
+        cout << "0" << endl;
+    }
+
+    return 0;
+}
