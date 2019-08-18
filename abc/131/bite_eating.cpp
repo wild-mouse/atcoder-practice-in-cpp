d61558c6d46cf1101fc2b1808e73075e9a67d02d
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-19 08:07:27 +0900

    Add Bite Eating

--- abc/131/bite_eating.cpp
+++ abc/131/bite_eating.cpp
@@ -1,4 +1,29 @@
-//
-// Created by 野口司 on 2019-08-19.
-//
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+    int n, l;
+    cin >> n >> l;
+    vector<int> a = vector<int>();
+    for (int i = 1; i <= n; i++) {
+        a.push_back(i + l - 1);
+    }
+    int minI = 0;
+    for (int i = 0; i < n; i++) {
+        if (abs(a[i]) < abs(a[minI])) {
+            minI = i;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (i == minI) {
+            continue;
+        }
+        sum += a[i];
+    }
+    cout << sum << endl;
+
+    return 0;
+}
