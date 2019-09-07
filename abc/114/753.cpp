2844f98740749f2e323b9d012fa829d8fe0f92ba
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-08 08:05:10 +0900

    Add 753

---
+++ abc/114/753.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x == 3 or x == 5 or x ==7) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
