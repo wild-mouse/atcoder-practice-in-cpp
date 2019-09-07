7445ef8a86f9ee5182063c32eb538026cc6362be
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-08 07:47:57 +0900

    Add Christmas Eve Eve Eve

---
+++ abc/115/christmas_eve_eve_eve.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int d;
+    cin >> d;
+    if (d == 22) {
+        cout << "Christmas Eve Eve Eve";
+    }
+    if (d == 23) {
+        cout << "Christmas Eve Eve";
+    }
+    if (d == 24) {
+        cout << "Christmas Eve";
+    }
+    if (d == 25) {
+        cout << "Christmas";
+    }
+}
