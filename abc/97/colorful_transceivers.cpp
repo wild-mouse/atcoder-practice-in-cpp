f37eeb31595d021b6ad791b563e94f30f5970d42
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-08 10:39:31 +0900

    Add Colorful Transceivers

---
+++ abc/97/colorful_transceivers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (abs(a - c) <= d || (abs(a - b) <= d && abs(b - c) <= d))
+    {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+}
