e580f2e6b8708c197421d2845f029d3de6d7c481
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-08 10:06:20 +0900

    Add Multiple of 2 and N

---
+++ abc/102/multiple_of_2_and_n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << n << "\n";
+    } else {
+        cout << n * 2 << "\n";
+    }
+}
