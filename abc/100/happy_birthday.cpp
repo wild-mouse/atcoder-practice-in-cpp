5f451eda15613ccb77b1a6feee31eb8212cb4e8a
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-08 10:15:45 +0900

    Add Happy Birthday

---
+++ abc/100/happy_birthday.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a <= 8 && b <= 8){
+        cout << "Yay!\n";
+    } else {
+        cout << ":(\n";
+    }
+}
